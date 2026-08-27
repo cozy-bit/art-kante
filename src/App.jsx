ae8aac464890d2b1b837a90b2ca2136d7e330797
Burhon010 <ilysovsamad@gmail.com>
2026-08-28 01:51:54 +0500

    fix: resolve broken merge in App.jsx (duplicate App, unclosed brace)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Concepts } from './pages/Concepts/Concepts'
 import { Home } from './pages/Home/Home'
 import { Philosophy } from './pages/Philosophy/Philosophy'
 import Project from './pages/Project/Project'
+import Design from './pages/Design/Design'
+import Favorites from './pages/Favorites/Favorites'
+import Portfolio from './pages/Portfolio/Portfolio'
+import Inspiration from './pages/Inspiration/Inspiration'
 
 function App() {
 	return (
@@ -11,6 +15,10 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Layout />}>
 					<Route index element={<Home />} />
+					<Route path='design' element={<Design />} />
+					<Route path='favorites' element={<Favorites />} />
+					<Route path='portfolio' element={<Portfolio />} />
+					<Route path='inspiration' element={<Inspiration />} />
 					<Route path='concepts' element={<Concepts />} />
 					<Route path='project' element={<Project />} />
 					<Route path='philosophy' element={<Philosophy />} />
@@ -18,28 +26,6 @@ function App() {
 			</Routes>
 		</BrowserRouter>
 	)
-import Design from './pages/Design/Design'
-import Favorites from './pages/Favorites/Favorites'
-import Portfolio from './pages/Portfolio/Portfolio'
-import Inspiration from './pages/Inspiration/Inspiration'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="design" element={<Design />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="inspiration" element={<Inspiration />} />
-          <Route path="concepts" element={<Concepts />} />
-          <Route path="project" element={<Project />} />
-          <Route path="philosophy" element={<Philosophy />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
 }
 
 export default App
